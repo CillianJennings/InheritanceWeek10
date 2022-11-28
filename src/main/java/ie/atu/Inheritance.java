d8ae9f42f9b5981a74fddf291f34776607ffcdad
Cillian <G00404411@atu.ie>
2022-11-28 09:36:54 +0000

    used the person class to make an instance for part 3

--- src/main/java/ie/atu/Inheritance.java
+++ src/main/java/ie/atu/Inheritance.java
@@ -1,6 +1,16 @@
 package ie.atu;
 
 public class Inheritance {
+    public static void main(String[] args) {
 
+        Person person1 = new Person();
+        person1.setName("Jack");
+        person1.setAddress("Galway");
+        person1.setTelephone("085420");
 
+        System.out.println("Persons name is: " + person1.getName());
+        System.out.println("Persons address is: " + person1.getAddress());
+        System.out.println("Persons telephone is: " + person1.getTelephone());
+
+    }
 }
